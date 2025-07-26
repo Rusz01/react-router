27f494bfb3fcb068c0feeaa1682a735f0b1069c8
rusz01 <ruszbaidhya@gmail.com>
2025-07-26 19:47:27 +0100

    Adding user, useParams

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 function User() {
+
+    const { userid } = useParams();
+
     return (
 
-        <div className="flex justify-center">User: </div>
+        <div className="flex justify-center content-center h-[50vh] mt-20 ">User: {userid} </div>
     )
 
 
